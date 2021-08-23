5077a78eda409ef6880a7650ab18fd33ab5da659
Rodrigo Costa <88461161+KaliRodri@users.noreply.github.com>
2021-08-23 03:36:23 +0000

    Minor changes

--- main.py
+++ main.py
@@ -5,17 +5,41 @@ palavras = ["Carro", "Moto", "Jogo", "Motor", "Subterraneo", "Araucarias", "Trat
 resposta = []
 
 
+import os
+import random
+from typing import Counter
+palavras = ["Carro", "Moto", "Jogo", "Motor", "Subterraneo", "Araucarias", "Trator", "Otorrino", "Laringologista"]
+senha = []
+
+
 while True:
   print("*****BEM VINDO*****")
   print("O que vai ser?""\n""1 - Nova Partida""\n""2 - Ver Pontuação""\n""3 - Sair")
+  
   choose = int(input())
   if choose == 1:
     print("Vamos começar!")
     os.system('cls')
+    
     pergunta = random.choice(palavras)
     resposta = list (pergunta)
     cod = "_" * len(resposta)
+    
     print(str(cod))
+    
+    senha.append (list(cod))
+    
+    print (senha)
+    
+    ("\n""Qual é a letra?")
+    
+    letra = input()
+    
+    if letra in resposta:
+     senha.append(letra)
+     print(senha)
+    else:
+     print("No, " + str(letra) +  " is not in list")
     
     
     
